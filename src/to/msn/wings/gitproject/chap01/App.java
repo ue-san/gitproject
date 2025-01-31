30bbc5590678713a0f89cd59ca8c0b9aa45cc096
ue-san <h.f.t.u0826@gmail.com>
2025-01-31 16:04:49 +0900

    Add files via upload

---
+++ src/to/msn/wings/gitproject/chap01/App.java
@@ -0,0 +1,13 @@
+package to.msn.wings.gitproject.chap01;
+
+import java.time.LocalDateTime;
+
+public class App {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+		String message = "Hello Java!";
+		System.out.println(message);
+	}
+}
